6efd47e81c7e6816d0943299c9c3a1ebeb793a0d
Jephter Maurice <mjephter2@gmail.com>
2023-07-21 18:12:20 -0700

    stash

---
+++ src/main/java/models/equations/QuadraticEquation.java
@@ -0,0 +1,58 @@
+package models.equations;
+
+import models.functions.Function;
+import models.functions.PolynomialFunction;
+import models.functions.PolynomialTerm;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+/**
+ * Class representing a quadratic equation.
+ */
+public class QuadraticEquation implements Equation {
+
+    public PolynomialFunction leftSide;
+    public PolynomialFunction rightSide;
+
+    public QuadraticEquation(final PolynomialFunction leftSide, final PolynomialFunction rightSide) {
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+
+        if (leftSide == null || rightSide == null) {
+            throw new IllegalArgumentException("Null argument(s) passed.");
+        }
+
+        final int leftSideDegree = Collections.max(leftSide.getTerms()
+                .stream()
+                .map(PolynomialTerm::getExponent)
+                .collect(Collectors.toList()));
+
+        final int rightSideDegree = Collections.max(rightSide.getTerms()
+                .stream()
+                .map(PolynomialTerm::getExponent)
+                .collect(Collectors.toList()));
+
+        if (leftSideDegree != 2 || rightSideDegree != 2) {
+            throw new IllegalArgumentException("Invalid degree(s) passed.");
+        }
+
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+    }
+
+    @Override
+    public Function leftSide() {
+        return this.leftSide;
+    }
+
+    @Override
+    public Function rightSide() {
+        return this.rightSide;
+    }
+
+    @Override
+    public double solve() {
+        return 0;
+    }
+}
