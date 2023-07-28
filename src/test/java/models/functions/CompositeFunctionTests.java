48deaac9d337a0afbb81857e47b4b7ca93760bf3
Jephter Maurice <mjephter2@gmail.com>
2023-07-27 18:07:26 -0700

    Add unit tests for CompositeFunction

--- src/test/java/models/functions/CompositeFunctionTests.java
+++ src/test/java/models/functions/CompositeFunctionTests.java
@@ -1,2 +1,51 @@
-package models.functions;public class CompositeFunctionTests {
+package models.functions;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CompositeFunctionTests {
+
+    @Test
+    public void testReduce() {
+        final PolynomialFunction factor1 = PolynomialFunction.builder()
+                .funcName("f")
+                .varName("x")
+                .terms(new LinkedList<>(){{
+                    add(new PolynomialTerm(1.0, "x", 1));
+                }})
+                .build();
+        final PolynomialFunction factor2 = PolynomialFunction.builder()
+                .funcName("g")
+                .varName("x")
+                .terms(new LinkedList<>(){{
+                    add(new PolynomialTerm(1.0, "x", 1));
+                    add(new PolynomialTerm(1.0, "x", 0));
+                }})
+                .build();
+        final PolynomialFunction factor3 = PolynomialFunction.builder()
+                .funcName("h")
+                .varName("x")
+                .terms(new LinkedList<>(){{
+                    add(new PolynomialTerm(1.0, "x", 1));
+                    add(new PolynomialTerm(-1.0, "x", 0));
+                }})
+                .build();
+
+        final LinkedList<PolynomialFunction> factors = new LinkedList<>(){{
+            add(factor1);
+            add(factor2);
+            add(factor3);
+        }};
+
+        final CompositeFunction compositeFunction = new CompositeFunction(factors);
+
+        assertEquals(factors, compositeFunction.factors);
+
+        final PolynomialFunction expectedReducedFunction = compositeFunction.reduce();
+
+        System.out.println(expectedReducedFunction.toString(false));
+    }
 }
