4d869956fd409086096a6215bd7cc4b9a55ff819
Jephter Maurice <mjephter2@gmail.com>
2023-08-01 19:28:16 -0700

    Add Circle class

---
+++ src/test/java/models/shapes/polygons/curves/CircleTests.java
@@ -0,0 +1,16 @@
+package models.shapes.polygons.curves;
+
+import models.shapes.curves.Circle;
+import models.shapes.curves.ClosedCurve;
+import org.junit.Test;
+
+public class CircleTests {
+
+    @Test
+    public void circle_succeeds() {
+        final ClosedCurve circle = new Circle(3);
+
+        assert circle.area() == 28.27;
+        assert circle.perimeter() == 18.85;
+    }
+}
