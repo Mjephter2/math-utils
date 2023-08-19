10aa49cf16c58865e49065dc8f853de0c889c87b
Jephter Maurice <mjephter2@gmail.com>
2023-08-19 12:31:10 -0700

    Add exponential function

---
+++ src/main/java/models/functions/ExponentialFunction.java
@@ -0,0 +1,84 @@
+package models.functions;
+
+import com.google.common.collect.Range;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Represents an exponential function
+ */
+@Builder
+@Getter
+public class ExponentialFunction implements Function {
+
+    private final double base;
+    private final String funcName;
+    private final String varName;
+    private final Function exponent;
+
+    @Override
+    public FunctionType getFuncType() {
+        return FunctionType.EXPONENTIAL;
+    }
+
+    @Override
+    public Range<Double> getDomain() {
+        return exponent.getDomain();
+    }
+
+    @Override
+    public Range<Double> getRange() {
+        throw new UnsupportedOperationException("Unimplemented method 'getRange'");
+    }
+
+    @Override
+    public double evaluate(Double... values) {
+        return Math.pow(base, exponent.evaluate(values));
+    }
+
+    @Override
+    // This implementation assumes that the base is a constant
+    public Function derivative() {
+        return new CompositeFunction(this.funcName, Stream.of(
+                exponent.derivative(),
+                PolynomialFunction.builder()
+                        .terms(new LinkedList<>() {{
+                            add(PolynomialTerm.builder()
+                                    .varName(exponent.getVarName())
+                                    .coefficient(Math.log(base))
+                                    .exponent(1)
+                                    .build());
+                        }})
+                        .funcName(this.funcName)
+                        .varName(this.varName)
+                        .build()
+        ).collect(Collectors.toList()));
+    }
+
+    @Override
+    public Function integral() {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'integral'");
+    }
+
+    @Override
+    public double integral(double lowerBound, double upperBound) {
+        // TODO
+        throw new UnsupportedOperationException("Unimplemented method 'integral'");
+    }
+
+    @Override
+    public double limit(double value) {
+        // TODO: Handle special cases
+        return Math.pow(base, exponent.limit(value));
+    }
+
+    @Override
+    public String printBody() {
+        return String.format("%f^%s", base, exponent.printFunc());
+    }
+}
