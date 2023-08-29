41429877c0db71b1f4a5e64161727e3666d549b2
Jephter Maurice <mjephter2@gmail.com>
2023-08-28 19:59:07 -0700

    Refactor SquareRootFunction into RadicalFunction

--- src/main/java/models/functions/radicals/SquareRootFunction.java
+++ src/main/java/models/functions/radicals/SquareRootFunction.java
@@ -15,108 +15,19 @@ import models.inequalities.LinearInequality;
 import java.util.Collections;
 
 /**
- * Class implementing a Radical function.
- * For now, implementing only square root
+ * Class implementing a Square Root function.
  */
 @Getter
-@Setter
-@Builder
-public class SquareRootFunction implements Function {
+public class SquareRootFunction extends RadicalFunction {
 
     private static final String SQUARE_ROOT = "√";
 
-    private static final double exponent = 0.5;
-
-    private final Function function;
-
-    public SquareRootFunction(final Function function) {
-        this.function = function;
-    }
-
-    @Override
-    public Range<Double> getDomain() {
-        return LinearInequality.builder()
-                .type(InequalityType.GREATER_THAN_OR_EQUAL_TO)
-                // TODO: Generalize RadicalFunction inner function to be generic Function
-                .leftSide((PolynomialFunction) function)
-                .rightSide(
-                        new PolynomialFunction(
-                                Collections.singletonList(new PolynomialTerm(0.0, "x", 0)),
-                                ((PolynomialFunction) function).getFuncName(),
-                                ((PolynomialFunction) function).getVarName())
-                )
-                .build().solve();
-    }
-
-    @Override
-    public Range<Double> getRange() {
-        return Range.atLeast(0.0);
-    }
-
-    @Override
-    public double evaluate(Double... values) {
-        return Math.pow(function.evaluate(values), exponent);
-    }
-
-    @Override
-    public Function derivative() {
-        throw new UnsupportedOperationException("Unimplemented method 'derivative'");
-    }
-
-    @Override
-    public Function integral() {
-        throw new UnsupportedOperationException("Unimplemented method 'integral'");
-    }
-
-    @Override
-    public double integral(double lowerBound, double upperBound) {
-        throw new UnsupportedOperationException("Unimplemented method 'integral'");
-    }
-
-    @Override
-    public double limit(double value) {
-        return Math.pow(function.limit(value), exponent);
-    }
-
-    @Override
-    public String toString() {
-        return this.printFunc();
+    SquareRootFunction(final String funcName, final String varName, final Function body) {
+        super(funcName, varName, 2, body);
     }
 
     @Override
     public String printBody() {
-        return SQUARE_ROOT + "(" + function.printBody() + ")";
-    }
-
-    @Override
-    public Function deepCopy() {
-        return new SquareRootFunction(function.deepCopy());
-    }
-
-    @Override
-    public String getFuncName() {
-        return this.function.getFuncName();
-    }
-
-    @Override
-    public String getVarName() {
-        return this.function.getVarName();
-    }
-
-    @Override
-    public FunctionType getFuncType() {
-        return FunctionType.RADICAL;
-    }
-
-    @Override
-    public boolean equals(final Object other) {
-        if (other.hashCode() == this.hashCode()) {
-            return true;
-        }
-        if (other instanceof SquareRootFunction) {
-            SquareRootFunction otherRadical = (SquareRootFunction) other;
-            return this.function.equals(otherRadical.function);
-        }
-        return false;
+        return SQUARE_ROOT + "(" + this.getBody().printBody() + ")";
     }
 }
