f9c6b771c22e2bf909b5640214dd9ccaeed8e54f
Jephter Maurice <mjephter2@gmail.com>
2023-09-02 14:50:12 -0700

    Implement limit for RadicalFunction #42

--- src/main/java/models/functions/radicals/RadicalFunction.java
+++ src/main/java/models/functions/radicals/RadicalFunction.java
@@ -3,9 +3,12 @@ package models.functions.radicals;
 import com.google.common.collect.Range;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import models.functions.Function;
 import models.functions.FunctionType;
 import utils.SuperscriptUtil;
+
+import java.util.logging.Logger;
 
 /**
  * Class implementing a Radical function
@@ -13,6 +16,7 @@ import utils.SuperscriptUtil;
 @Getter
 @Builder
 public class RadicalFunction implements Function {
+    private static final Logger logger = Logger.getLogger(RadicalFunction.class.getName());
 
     private final String funcName;
     private final String varName;
@@ -71,8 +75,12 @@ public class RadicalFunction implements Function {
 
     @Override
     public double limit(double value) {
-        // TODO: Implement limit for RadicalFunction
-        throw new UnsupportedOperationException("Unimplemented method 'limit'");
+        if (this.getDomain().contains(value)) {
+            return this.evaluate(value);
+        } else {
+            logger.info("Limit for function " + this + " at value " + value + " is undefined");
+            return Double.NaN;
+        }
     }
 
     @Override
