ed5b49bb9fb6da35e81d2d45c7c86352b7491eb5
Jephter Maurice <mjephter2@gmail.com>
2023-09-06 19:03:29 -0700

    Add NaturalLogEquation

---
+++ src/main/java/models/equations/log_equations/NaturalLogEquation.java
@@ -0,0 +1,14 @@
+package models.equations.log_equations;
+
+import models.functions.Function;
+import models.functions.logarithmic.NaturalLogFunction;
+
+/**
+ * Represents an equation with a natural log function on the left side and a function on the right side
+ */
+public class NaturalLogEquation extends LogEquation {
+
+    NaturalLogEquation(final NaturalLogFunction leftSide, final Function rightSide) {
+        super(leftSide, rightSide);
+    }
+}
