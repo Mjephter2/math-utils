6588843db20b7574d4f5eb1f0cc46089999cde57
Jephter Maurice <mjephter2@gmail.com>
2023-09-19 19:25:09 -0700

    Improve coverage for SquareRootEquation

--- src/test/java/models/equations/SquareRootEquationTests.java
+++ src/test/java/models/equations/SquareRootEquationTests.java
@@ -3,11 +3,13 @@ package models.equations;
 import models.functions.polynomials.PolynomialFunction;
 import models.functions.polynomials.PolynomialTerm;
 import models.functions.radicals.SquareRootFunction;
+import models.functions.trigonometric.TrigonometricFunction;
 import org.junit.Test;
 
 import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class SquareRootEquationTests {
 
@@ -49,5 +51,66 @@ public class SquareRootEquationTests {
 
         assertEquals(1, solution.length);
         assertEquals(1.0, solution[0], 0.0);
+    }
+
+    /**
+     * Case where both left and right side are square root functions of a polynomial
+     */
+    @Test
+    public void case2_tests() {
+        final SquareRootFunction lhs = new SquareRootFunction("g", "x",
+                new PolynomialFunction(new LinkedList<>(){{
+                    add(PolynomialTerm.builder()
+                            .coefficient(1.0)
+                            .varName("x")
+                            .exponent(2)
+                            .build());
+                }}, "f", "x"));
+        final SquareRootFunction rhs = new SquareRootFunction("h", "x",
+                new PolynomialFunction(new LinkedList<>(){{
+                    add(PolynomialTerm.builder()
+                            .coefficient(1.0)
+                            .varName("x")
+                            .exponent(1)
+                            .build());
+                }}, "f", "x"));
+
+        final SquareRootEquation equation = SquareRootEquation.builder()
+                .leftSide(lhs)
+                .rightSide(rhs)
+                .build();
+
+        final Double[] solution = equation.solve();
+
+        assertEquals(lhs, equation.getLeftSide());
+        assertEquals(rhs, equation.getRightSide());
+
+        assertEquals(2, solution.length);
+
+        assertEquals(1.0, solution[0], 0.0);
+        assertEquals(0.0, solution[1], 0.0);
+
+        System.out.println(solution[0]);
+        System.out.println(solution[1]);
+    }
+
+    @Test
+    public void exceptions_tests() {
+        final SquareRootFunction lhs = new SquareRootFunction("g", "x",
+                new PolynomialFunction(new LinkedList<>(){{
+                    add(PolynomialTerm.builder()
+                            .coefficient(1.0)
+                            .varName("x")
+                            .exponent(2)
+                            .build());
+                }}, "f", "x"));
+        final TrigonometricFunction trigFunc = TrigonometricFunction.builder().build();
+
+        assertThrows(UnsupportedOperationException.class, () -> SquareRootEquation.builder()
+                .leftSide(lhs)
+                .rightSide(trigFunc)
+                .build()
+                .solve()
+        );
     }
 }
