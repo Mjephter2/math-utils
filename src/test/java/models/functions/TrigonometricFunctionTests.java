bf9266e6e04e40ea854bc0a8cc58484b5732efa9
Jephter Maurice <mjephter2@gmail.com>
2023-10-03 19:11:05 -0700

    Add unit tests for sine function

---
+++ src/test/java/models/functions/TrigonometricFunctionTests.java
@@ -0,0 +1,43 @@
+package models.functions;
+
+import models.functions.polynomials.PolynomialFunction;
+import models.functions.polynomials.PolynomialTerm;
+import models.functions.trigonometric.TrigonometricFunction;
+import models.functions.trigonometric.TrigonometricFunctionType;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrigonometricFunctionTests {
+
+    @Test
+    public void sin_tests() {
+        final PolynomialFunction innerFunction = new PolynomialFunction(new LinkedList<PolynomialTerm>() {{
+            add(PolynomialTerm.builder()
+                    .coefficient(1.0)
+                    .varName("x")
+                    .exponent(1)
+                    .build());
+        }}, "i", "x");
+        final TrigonometricFunction sinFunc = TrigonometricFunction.builder()
+                .funcName("f")
+                .varName("x")
+                .trigonometricFunctionType(TrigonometricFunctionType.SINE)
+                .innerFunction(innerFunction)
+                .build();
+
+        assertEquals("f(x) = sin(x)", sinFunc.printFunc());
+        assertEquals("f", sinFunc.getFuncName());
+        assertEquals("x", sinFunc.getVarName());
+        assertEquals(TrigonometricFunctionType.SINE, sinFunc.getTrigonometricFunctionType());
+        assertEquals(innerFunction, sinFunc.getInnerFunction());
+
+        assertEquals(0.0, sinFunc.evaluate(0.0), 0.0);
+        assertEquals(1.0, sinFunc.evaluate(Math.PI / 2), 0.0);
+        assertEquals(0.0, sinFunc.evaluate(Math.PI), 0.00001);
+        assertEquals(-1.0, sinFunc.evaluate(3 * Math.PI / 2), 0.0);
+        assertEquals(0.0, sinFunc.evaluate(2 * Math.PI), 0.00001);
+    }
+}
