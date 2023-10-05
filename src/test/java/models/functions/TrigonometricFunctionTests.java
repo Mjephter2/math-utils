470d7ee95f648e60d3a27ccf3153fb3b33fa9429
Jephter Maurice <mjephter2@gmail.com>
2023-10-04 19:46:31 -0700

    Add unit tests for cosine function

--- src/test/java/models/functions/TrigonometricFunctionTests.java
+++ src/test/java/models/functions/TrigonometricFunctionTests.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 public class TrigonometricFunctionTests {
 
     @Test
-    public void sin_tests() {
+    public void sine_tests() {
         final PolynomialFunction innerFunction = new PolynomialFunction(new LinkedList<PolynomialTerm>() {{
             add(PolynomialTerm.builder()
                     .coefficient(1.0)
@@ -39,5 +39,34 @@ public class TrigonometricFunctionTests {
         assertEquals(0.0, sinFunc.evaluate(Math.PI), 0.00001);
         assertEquals(-1.0, sinFunc.evaluate(3 * Math.PI / 2), 0.0);
         assertEquals(0.0, sinFunc.evaluate(2 * Math.PI), 0.00001);
+    }
+
+    @Test
+    public void cosine_tests() {
+        final PolynomialFunction innerFunction = new PolynomialFunction(new LinkedList<PolynomialTerm>() {{
+            add(PolynomialTerm.builder()
+                    .coefficient(1.0)
+                    .varName("x")
+                    .exponent(1)
+                    .build());
+        }}, "i", "x");
+        final TrigonometricFunction sinFunc = TrigonometricFunction.builder()
+                .funcName("f")
+                .varName("x")
+                .trigonometricFunctionType(TrigonometricFunctionType.COSINE)
+                .innerFunction(innerFunction)
+                .build();
+
+        assertEquals("f(x) = cos(x)", sinFunc.printFunc());
+        assertEquals("f", sinFunc.getFuncName());
+        assertEquals("x", sinFunc.getVarName());
+        assertEquals(TrigonometricFunctionType.COSINE, sinFunc.getTrigonometricFunctionType());
+        assertEquals(innerFunction, sinFunc.getInnerFunction());
+
+        assertEquals(1.0, sinFunc.evaluate(0.0), 0.0);
+        assertEquals(0.0, sinFunc.evaluate(Math.PI / 2), 0.00001);
+        assertEquals(-1.0, sinFunc.evaluate(Math.PI), 0.0);
+        assertEquals(0.0, sinFunc.evaluate(3 * Math.PI / 2), 0.00001);
+        assertEquals(1.0, sinFunc.evaluate(2 * Math.PI), 0.0);
     }
 }
