6dccdf789afdfe20f122fc9d6c2b934f20bb5f9a
Jephter Maurice <mjephter2@gmail.com>
2023-10-14 11:30:11 -0700

    Add generic inequality

---
+++ src/main/java/models/inequalities/PolynomialInequality.java
@@ -0,0 +1,26 @@
+package models.inequalities;
+
+import com.google.common.collect.Range;
+import lombok.Builder;
+import lombok.Getter;
+import models.functions.polynomials.PolynomialFunction;
+
+import java.util.List;
+
+@Getter
+@Builder
+class PolynomialInequality implements Inequality {
+
+    private PolynomialFunction leftSide;
+    private PolynomialFunction rightSide;
+    private InequalityType type;
+
+    @Override
+    public List<Range<Double>> solve() {
+        // If both sides are linear equations, solve as a linear inequality
+        if (leftSide.getDegree() == 1 && rightSide.getDegree() == 1) {
+            return new LinearInequality(this.type, this.leftSide, this.rightSide).solve();
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'solve'");
+    }
+}
