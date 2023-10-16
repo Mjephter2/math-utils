b9352d3d4e127cef3f774a5823ddd428c68604fe
Jephter Maurice <mjephter2@gmail.com>
2023-10-16 02:52:49 -0700

    Add unit tests for InequalityType

---
+++ src/test/java/models/inequalities/InequalityTypeTests.java
@@ -0,0 +1,30 @@
+package models.inequalities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class InequalityTypeTests {
+
+    @Test
+    public void from_tests() {
+        assertEquals(InequalityType.LESS_THAN, InequalityType.fromSymbol("<"));
+        assertEquals(InequalityType.LESS_THAN_OR_EQUAL_TO, InequalityType.fromSymbol("<="));
+        assertEquals(InequalityType.GREATER_THAN, InequalityType.fromSymbol(">"));
+        assertEquals(InequalityType.GREATER_THAN_OR_EQUAL_TO, InequalityType.fromSymbol(">="));
+        assertEquals(InequalityType.EQUAL_TO, InequalityType.fromSymbol("="));
+        assertEquals(InequalityType.NOT_EQUAL_TO, InequalityType.fromSymbol("!="));
+        assertNull(InequalityType.fromSymbol("invalid"));
+    }
+
+    @Test
+    public void reverse_tests() {
+        assertEquals(InequalityType.GREATER_THAN, InequalityType.reverse(InequalityType.LESS_THAN));
+        assertEquals(InequalityType.GREATER_THAN_OR_EQUAL_TO, InequalityType.reverse(InequalityType.LESS_THAN_OR_EQUAL_TO));
+        assertEquals(InequalityType.LESS_THAN, InequalityType.reverse(InequalityType.GREATER_THAN));
+        assertEquals(InequalityType.LESS_THAN_OR_EQUAL_TO, InequalityType.reverse(InequalityType.GREATER_THAN_OR_EQUAL_TO));
+        assertEquals(InequalityType.NOT_EQUAL_TO, InequalityType.reverse(InequalityType.EQUAL_TO));
+        assertEquals(InequalityType.EQUAL_TO, InequalityType.reverse(InequalityType.NOT_EQUAL_TO));
+    }
+}
