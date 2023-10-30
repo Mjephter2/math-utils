0ece38f505eca9297d75dda5d280dae59e50d02d
Jephter Maurice <mjephter2@gmail.com>
2023-10-30 06:57:52 -0700

    Add derivativeUtils productRule unit tests

---
+++ src/test/java/models/utils/DerivativeUtilsTests.java
@@ -0,0 +1,58 @@
+package models.utils;
+
+import models.functions.Function;
+import models.functions.polynomials.PolynomialFunction;
+import models.functions.polynomials.PolynomialTerm;
+import org.junit.Test;
+import utils.derivativeUtils;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class DerivativeUtilsTests {
+
+    @Test
+    public void productRuleTest() {
+        final Function f1 = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(2)
+                    .varName("x")
+                    .exponent(3)
+                    .build());
+        }}, "f1", "x");
+        final Function f2 = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(2)
+                    .varName("x")
+                    .exponent(2)
+                    .build());
+        }}, "f2", "x");
+
+        // Compute derivative of f1 * f2
+        final Function derivative = derivativeUtils.productRule(f1, f2);
+
+        assertEquals("(f1 * f2)'(x) = ( 6x² )( 2x² ) + ( 2x³ )( 4x )", derivative.printFunc());
+    }
+
+    @Test
+    public void productTestExceptionsTests() {
+        final Function f1 = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(2)
+                    .varName("x")
+                    .exponent(3)
+                    .build());
+        }}, "f1", "x");
+        final Function f2 = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(2)
+                    .varName("y")
+                    .exponent(2)
+                    .build());
+        }}, "f2", "y");
+
+        assertThrows(IllegalArgumentException.class, () -> derivativeUtils.productRule(f1, f2));
+    }
+}
