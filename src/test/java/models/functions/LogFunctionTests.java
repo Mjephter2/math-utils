fe7449845da14349fa1272edd39f38b95eba13cd
Jephter Maurice <mjephter2@gmail.com>
2023-11-13 18:02:51 -0800

    Add exceptions unit tests for LogFunction

--- src/test/java/models/functions/LogFunctionTests.java
+++ src/test/java/models/functions/LogFunctionTests.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class LogFunctionTests {
 
@@ -39,6 +40,7 @@ public class LogFunctionTests {
         assertEquals(Range.open(0.0, Double.POSITIVE_INFINITY), logFunction.getRange());
 
         assertEquals(2.0, logFunction.evaluate(5.0), 0.0);
+        assertEquals(2.0, logFunction.limit(5.0), 0.0);
     }
 
     @Test
@@ -73,5 +75,23 @@ public class LogFunctionTests {
         final Function simplified2 = logFunction2.simplify();
 
         System.out.println(simplified2);
+    }
+
+    @Test
+    public void exceptionsTest() {
+        final ConstantFunction innerFunc = ConstantFunction.builder()
+                .value(10)
+                .build();
+        final LogFunction logFunction = LogFunction.builder()
+                .funcName("f")
+                .varName("x")
+                .body(innerFunc)
+                .base(10)
+                .build();
+
+        assertThrows(UnsupportedOperationException.class, logFunction::derivative);
+        assertThrows(UnsupportedOperationException.class, logFunction::getDomain);
+        assertThrows(UnsupportedOperationException.class, logFunction::integral);
+        assertThrows(UnsupportedOperationException.class, () -> logFunction.integral(1.0, 2.0));
     }
 }
