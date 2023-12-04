ed1722fd00c7c988b0e9162029ed8e960ebb3be9
Jephter Maurice <mjephter2@gmail.com>
2023-12-04 06:19:54 -0800

    Add Range class to present ranges of numbers

---
+++ src/main/java/models/numberUtils/Range.java
@@ -0,0 +1,115 @@
+package models.numberUtils;
+
+import lombok.Getter;
+
+/**
+ *
+ */
+@Getter
+public class Range implements Comparable {
+
+    private Double lowerBound;
+    private boolean includeLowerBound;
+
+    private Double upperBound;
+    private boolean includeUpperBound;
+
+    public Range(final Double lowerBound, final Double uppperBound, final boolean includeLowerBound, final boolean includeUpperBound) {
+        if (validatedBounds(lowerBound, uppperBound, includeLowerBound, includeUpperBound)) {
+            this.lowerBound = lowerBound;
+            this.upperBound = uppperBound;
+            this.includeLowerBound = includeLowerBound;
+            this.includeUpperBound = includeUpperBound;
+        } else {
+            throw new IllegalArgumentException("Invalid bounds values provided");
+        }
+    }
+
+    private boolean validatedBounds(final Double lowerBound, final Double uppperBound, final boolean includeLowerBound, final boolean includeUpperBound) {
+        if ((includeLowerBound && lowerBound.isInfinite()) || (includeUpperBound && uppperBound.isInfinite())) {
+            return false;
+        }
+        return true;
+    }
+
+    public static Range atLeast(final double lowerBound) {
+        return new Range(lowerBound, Double.POSITIVE_INFINITY, true, false);
+    }
+
+    public static Range greaterThan(final double lowerBound) {
+        return new Range(lowerBound, Double.POSITIVE_INFINITY, false, false);
+    }
+
+    public static Range atMost(final double upperBound) {
+        return new Range(Double.NEGATIVE_INFINITY, upperBound, false, true);
+    }
+
+    public static Range lessThan(final double upperBound) {
+        return new Range(Double.NEGATIVE_INFINITY, upperBound, false, false);
+    }
+
+    public static Range all() {
+        return new Range(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false, false);
+    }
+
+    public static Range allPositive(final boolean includeZero) {
+        return new Range(0.0, Double.POSITIVE_INFINITY, includeZero, false);
+    }
+
+    public static Range allNegative(final boolean includeZero) {
+        return new Range(Double.NEGATIVE_INFINITY, 0.0, false, includeZero);
+    }
+
+    public boolean includes(final Double value) {
+        if (value.equals(Double.POSITIVE_INFINITY)) {
+            return this.upperBound.equals(Double.POSITIVE_INFINITY);
+        } else if (value.equals(Double.NEGATIVE_INFINITY)) {
+            return this.lowerBound.equals(Double.NEGATIVE_INFINITY);
+        }
+
+        return (this.includeLowerBound ? value >= this.lowerBound : value > this.lowerBound) &&
+                (this.includeUpperBound ? value <= this.upperBound : value < this.upperBound);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder rep = new StringBuilder("Range::").append(includeLowerBound ? "[" : "(");
+        rep.append(printBound(this.lowerBound)).append(" --> ").append(printBound(this.upperBound));
+        rep.append(this.includeUpperBound ? "]" : ")");
+
+        return rep.toString();
+    }
+
+    private String printBound(final Double value) {
+        if (value.equals(Double.POSITIVE_INFINITY)) {
+            return "∞";
+        } else if (value.equals(Double.NEGATIVE_INFINITY)) {
+            return "-∞";
+        }
+
+        return value.toString();
+    }
+
+    public static void main(String[] args) {
+        final Range range = Range.allPositive(true);
+
+        System.out.println(range.includes(-1000.0));
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Range) {
+            final Range other = (Range) o;
+            if (this.lowerBound.equals(other.lowerBound)) {
+                if (this.upperBound.equals(other.upperBound)) {
+                    return 0;
+                } else {
+                    return this.upperBound.compareTo(other.upperBound);
+                }
+            } else {
+                return this.lowerBound.compareTo(other.lowerBound);
+            }
+        }
+        return -1;
+    }
+}
