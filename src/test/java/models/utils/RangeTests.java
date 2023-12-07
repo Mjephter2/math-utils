690332aaed88ea4621dfe417ad2fbe9885723327
Jephter Maurice <mjephter2@gmail.com>
2023-12-07 06:33:04 -0800

    Add Range atMost unit tests

--- src/test/java/models/utils/RangeTests.java
+++ src/test/java/models/utils/RangeTests.java
@@ -21,4 +21,17 @@ public class RangeTests {
         assertTrue(range1.includes(0.0));
         assertFalse(range1.includes(-1.1));
     }
+
+    @Test
+    public void atMostTests() {
+        final Range range1 = Range.atMost(0.0);
+
+        assertEquals(0.0, range1.getUpperBound());
+        assertTrue(range1.isIncludeUpperBound());
+        assertEquals(Double.NEGATIVE_INFINITY, range1.getLowerBound());
+        assertTrue(range1.isIncludeUpperBound());
+
+        assertTrue(range1.includes(0.0));
+        assertFalse(range1.includes(1.1));
+    }
 }
