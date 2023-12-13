45dd3152342de73a3d4c30b2b2e3f29a2cead990
Jephter Maurice <mjephter2@gmail.com>
2023-12-12 20:20:46 -0800

    Add more Range methods add Java doc

--- src/main/java/models/numberUtils/Range.java
+++ src/main/java/models/numberUtils/Range.java
@@ -5,19 +5,36 @@ import lombok.Getter;
 import java.util.List;
 
 /**
- *
+ * A class representing a range of numbers
  */
 @Getter
 public class Range implements Comparable {
 
+    /**
+     * The lower bound of the range
+     */
     private Double lowerBound;
+    /**
+     * Whether the lower bound is included in the range
+     */
     private boolean includeLowerBound;
 
+    /**
+     * The upper bound of the range
+     */
     private Double upperBound;
+    /**
+     * Whether the upper bound is included in the range
+     */
     private boolean includeUpperBound;
 
-    // made public for testability
-    // this constructor should technically be private
+    /**
+     * Constructs a range of numbers
+     * @param lowerBound - the lower bound of the range
+     * @param uppperBound - the upper bound of the range
+     * @param includeLowerBound - whether the lower bound is included in the range
+     * @param includeUpperBound - whether the upper bound is included in the range
+     */
     public Range(final Double lowerBound, final Double uppperBound, final boolean includeLowerBound, final boolean includeUpperBound) {
         if (validatedBounds(lowerBound, uppperBound, includeLowerBound, includeUpperBound)) {
             this.lowerBound = lowerBound;
@@ -36,34 +53,106 @@ public class Range implements Comparable {
         return true;
     }
 
+    /**
+     * Returns a range of all numbers equal or greater than a specified lower bound
+     * @param lowerBound - the lower bound of the range
+     * @return a range of all numbers equal or greater than the lower bound
+     */
     public static Range atLeast(final double lowerBound) {
         return new Range(lowerBound, Double.POSITIVE_INFINITY, true, false);
     }
 
+    /**
+     * Returns a range of all numbers strictly greater than a specified lower bound
+     * @param lowerBound - the lower bound of the range
+     * @return a range of all numbers strictly greater than the lower bound
+     */
     public static Range greaterThan(final double lowerBound) {
         return new Range(lowerBound, Double.POSITIVE_INFINITY, false, false);
     }
 
+    /**
+     * Returns a range of all numbers less than or equal to a specified upper bound
+     * @param upperBound - the upper bound of the range
+     * @return a range of all numbers less than or equal to the upper bound
+     */
     public static Range atMost(final double upperBound) {
         return new Range(Double.NEGATIVE_INFINITY, upperBound, false, true);
     }
 
+    /**
+     * Returns a range of all numbers strictly less than a specified upper bound
+     * @param upperBound - the upper bound of the range
+     * @return a range of all numbers strictly less than the upper bound
+     */
     public static Range lessThan(final double upperBound) {
         return new Range(Double.NEGATIVE_INFINITY, upperBound, false, false);
     }
 
+    /**
+     * Returns a range of all Double values
+     */
     public static Range all() {
         return new Range(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false, false);
     }
 
+    /**
+     * Returns a range that excludes a single value
+     * @param exceptedValue - the value to exclude
+     * @return a list of two ranges that excludes the value
+     */
+    public static List<Range> allExcept(final Double exceptedValue) {
+        return List.of(
+                new Range(Double.NEGATIVE_INFINITY, exceptedValue, false, false),
+                new Range(exceptedValue, Double.POSITIVE_INFINITY, false, false)
+        );
+    }
+
+    /**
+     * Returns a range that excludes a range
+     * @param exceptedRange - the range to exclude
+     * @return a list of two ranges that excludes the range
+     */
+    public static List<Range> allExcept(final Range exceptedRange) {
+        return List.of(
+                new Range(Double.NEGATIVE_INFINITY, exceptedRange.getLowerBound(), false, !exceptedRange.isIncludeUpperBound()),
+                new Range(exceptedRange.getUpperBound(), Double.POSITIVE_INFINITY, !exceptedRange.isIncludeUpperBound(), false)
+        );
+    }
+
+    /**
+     * Returns a range that only consists of a single value
+     * @param value - the value to include
+     * @return a range that includes the value
+     */
+    public static Range singleton(final Double value) {
+        return new Range(value, value, true, true);
+    }
+
+    /**
+     * Returns a range that includes all positive numbers
+     * @param includeZero - whether to include zero in the range
+     * @return a range that includes all positive numbers except maybe zero
+     */
     public static Range allPositive(final boolean includeZero) {
         return new Range(0.0, Double.POSITIVE_INFINITY, includeZero, false);
     }
 
+    /**
+     * Returns a range that includes all negative numbers
+     * @param includeZero - whether to include zero in the range
+     * @return a range that includes all negative numbers except maybe zero
+     */
     public static Range allNegative(final boolean includeZero) {
         return new Range(Double.NEGATIVE_INFINITY, 0.0, false, includeZero);
     }
 
+    /**
+     * Returns whether this range includes a value
+     * @param value - the value to check
+     * @return true when the value is included in this range
+     *       false otherwise
+     */
     public boolean includes(final Double value) {
         if (value.equals(Double.POSITIVE_INFINITY)) {
             return this.upperBound.equals(Double.POSITIVE_INFINITY);
@@ -75,14 +164,49 @@ public class Range implements Comparable {
                 (this.includeUpperBound ? value <= this.upperBound : value < this.upperBound);
     }
 
+    /**
+     * Returns whether this range includes all the values in another range
+     * @param other - the other range to check values against
+     * @return true when all  values of @other are included in this range
+     *        false otherwise
+     */
     public boolean includes(final Range other) {
         return this.includes(other.lowerBound) && this.includes(other.upperBound);
     }
 
+    /**
+     * Returns the overlap of this range with another range
+     * @param other
+     * @return the common range between this range and the other range
+     */
+    public Range intersection(final Range other) {
+        if (this.includes(other)) {
+            return other;
+        } else if (other.includes(this)) {
+            return this;
+        } else if (this.includes(other.lowerBound)) {
+            return new Range(other.lowerBound, this.upperBound, other.includeLowerBound, this.includeUpperBound);
+        } else if (this.includes(other.upperBound)) {
+            return new Range(this.lowerBound, other.upperBound, this.includeLowerBound, other.includeUpperBound);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns whether this range overlaps with another range
+     * @param other - the other range to check for overlap
+     * @return true when the ranges overlap
+     *       false otherwise
+     */
     public boolean overlaps(final Range other) {
         return this.includes(other.lowerBound) || this.includes(other.upperBound);
     }
 
+    /**
+     * Returns a string representation of this range
+     * @return a string of the form `Range::[lowerBound --> upperBound]`
+     */
     @Override
     public String toString() {
         final StringBuilder rep = new StringBuilder("Range::").append(includeLowerBound ? "[" : "(");
@@ -102,6 +226,11 @@ public class Range implements Comparable {
         return value.toString();
     }
 
+    /**
+     * Returns whether this range is equal to another range
+     * @param o the object to be compared.
+     * @return true when the ranges have the same bounds and include flags
+     */
     @Override
     public int compareTo(final Object o) {
         if (o instanceof Range) {
@@ -140,7 +269,9 @@ public class Range implements Comparable {
         }
     }
 
-    // Deep copy this range
+    /**
+     * Returns a deep copy of this range
+     */
     public Range deepCopy() {
         return new Range(this.lowerBound, this.upperBound, this.includeLowerBound, this.includeUpperBound);
     }
