a7ff10002b8b33948b4da90823cec05ee65a0bfa
Jephter Maurice <mjephter2@gmail.com>
2024-01-08 16:14:54 -0800

    Add QuadraticInequality class and implement solver

--- src/test/java/models/inequalities/PolynomialInequalityTests.java
+++ src/test/java/models/inequalities/PolynomialInequalityTests.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PolynomialInequalityTests {
 
@@ -44,6 +45,52 @@ public class PolynomialInequalityTests {
         List<Range> solution = inequality.getSolution();
 
         assertEquals(1, solution.size());
+    }
+
+    @Test
+    public void quadraticInequalityTest() {
+        final PolynomialFunction leftSide = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(1.0)
+                    .varName("x")
+                    .exponent(5)
+                    .build());
+        }}, "f", "x");
+        final PolynomialFunction rightSide = new PolynomialFunction(new LinkedList<>(){{
+            add(PolynomialTerm.builder()
+                    .coefficient(1.0)
+                    .varName("x")
+                    .exponent(1)
+                    .build());
+        }}, "g", "x");
+
+        final QuadraticInequality inequality1 = new QuadraticInequality(InequalityType.GREATER_THAN, leftSide, rightSide);
+        assertEquals(InequalityType.GREATER_THAN, inequality1.getType());
+        assertEquals(leftSide, inequality1.getLeftSide());
+        assertEquals(rightSide, inequality1.getRightSide());
+        assertEquals(5, inequality1.getDegree());
+
+        inequality1.solve();
+        List<Range> solution = inequality1.getSolution();
+
+        assertEquals(2, solution.size());
+        assertTrue(solution.contains(new Range(1.0, Double.POSITIVE_INFINITY, false, false)));
+        assertTrue(solution.contains(new Range(-1.0, 0.0, false, false)));
+
+        final QuadraticInequality inequality2 = new QuadraticInequality(InequalityType.LESS_THAN, leftSide, rightSide);
+        assertEquals(InequalityType.LESS_THAN, inequality2.getType());
+        assertEquals(leftSide, inequality2.getLeftSide());
+        assertEquals(rightSide, inequality2.getRightSide());
+
+        inequality2.solve();
+        List<Range> solution2 = inequality2.getSolution();
+
+        assertEquals(2, solution2.size());
+        assertTrue(solution2.contains(new Range(Double.NEGATIVE_INFINITY, -1.0, false, false)));
+        assertTrue(solution2.contains(new Range(0.0, 1.0, false, false)));
+
+        assertThrows(IllegalArgumentException.class, () -> new QuadraticInequality(InequalityType.LESS_THAN, null, rightSide));
+        assertThrows(IllegalArgumentException.class, () -> new QuadraticInequality(InequalityType.LESS_THAN, leftSide, null));
     }
 
     @Test
