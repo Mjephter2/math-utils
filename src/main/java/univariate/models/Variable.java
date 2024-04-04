ebde9707f9878a079f7191b7aa5a7b805601fc9f
Jephter Maurice <mjephter2@gmail.com>
2024-04-03 20:47:03 -0700

    Enhance Variable class to implement Comparable

--- src/main/java/univariate/models/Variable.java
+++ src/main/java/univariate/models/Variable.java
@@ -1,11 +1,12 @@
 package univariate.models;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
 
-
+@EqualsAndHashCode
 @Getter
-public class Variable {
+public class Variable implements Comparable<Variable> {
     @NonNull
     private final String name;
 
@@ -15,5 +16,10 @@ public class Variable {
 
     public static Variable defaultVariable() {
         return new Variable("x");
+    }
+
+    @Override
+    public int compareTo(Variable o) {
+        return this.name.compareTo(o.name);
     }
 }
